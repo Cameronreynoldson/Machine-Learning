dabb97ba14bae54b732982ef6f2b24e158682f08
Cameron Reynoldson <cameronreynoldson@utexas.edu>
2015-06-25 23:45:54 -0500

    more fixes to accuracy, but bad data set in general

--- Blog-Comment-Prediction/CommentPrediction.py
+++ Blog-Comment-Prediction/CommentPrediction.py
@@ -3,14 +3,11 @@ import numpy as np
 def gradient_step(theta,features,learning_rate,targets):
 	m = features.size;
 	temp_theta = np.array([0]*48)
-	print(features.shape)
-	print(theta.shape)
-	prediction = features.dot(theta)
+	prediction = features.dot(np.transpose(theta))
 	for i in range(0,len(theta)): 
 		error_sum = 0
 		for j in range(0,features[:,i].size):
 			error_sum += (prediction[i]-targets[i])*features[i:i,j:j]
-			print(error_sum)
 		temp_theta_val = (theta[i] - (learning_rate/m)*error_sum)
 	return temp_theta
 
@@ -28,7 +25,6 @@ def run():
 	features[0] = 1
 	num_iterations = 1000
 	theta = linear_regression(theta,targets,features,learning_rate,num_iterations)
-	print(theta)
 
 if __name__ == '__main__':
 	run()
