c1ddefea873c8e0af83d072c56ea1d7f41b4c322
Cameron Reynoldson <cameronreynoldson@utexas.edu>
2015-06-25 23:19:28 -0500

    most of gradient descent done

---
+++ Blog-Comment-Prediction/CommentPrediction.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def gradient_step(theta,features,learning_rate,targets):
+	m = features.size;
+	temp_theta = np.array([0]*48)
+	print(features.shape)
+	print(theta.shape)
+	prediction = features.dot(theta)
+	for i in range(0,len(theta)): 
+		error_sum = 0
+		for j in range(0,features[:,i].size):
+			error_sum += (prediction[i]-targets[i])*features[i:i,j:j]
+			print(error_sum)
+		temp_theta_val = (theta[i] - (learning_rate/m)*error_sum)
+	return temp_theta
+
+def linear_regression(theta,targets,features,learning_rate,num_iterations):
+	for i in range(0,num_iterations):
+		theta = gradient_step(theta,features,learning_rate,targets)
+	return theta
+
+def run():
+	data = np.genfromtxt("example.csv", delimiter=",")
+	learning_rate = 0.0001
+	theta = np.array([0]*48)
+	targets = np.array(data[:,280])
+	features = np.array(data[:,0:48])
+	features[0] = 1
+	num_iterations = 1000
+	theta = linear_regression(theta,targets,features,learning_rate,num_iterations)
+	print(theta)
+
+if __name__ == '__main__':
+	run()
