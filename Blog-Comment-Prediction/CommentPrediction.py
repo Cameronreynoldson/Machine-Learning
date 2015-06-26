b24c739c44db50e999591248b95546201fb3c1b8
Cameron Reynoldson <cameronreynoldson@utexas.edu>
2015-06-25 23:53:23 -0500

    fixed indexing issues

--- Blog-Comment-Prediction/CommentPrediction.py
+++ Blog-Comment-Prediction/CommentPrediction.py
@@ -4,11 +4,12 @@ def gradient_step(theta,features,learning_rate,targets):
 	m = features.size;
 	temp_theta = np.array([0]*48)
 	prediction = features.dot(np.transpose(theta))
-	for i in range(0,len(theta)): 
+	for i in range(0,47): 
 		error_sum = 0
-		for j in range(0,features[:,i].size):
-			error_sum += (prediction[i]-targets[i])*features[i:i,j:j]
+		for j in range(0,109):
+			error_sum += (prediction[i]-targets[i])*features[j,i]
 		temp_theta_val = (theta[i] - (learning_rate/m)*error_sum)
+		temp_theta[i] = temp_theta_val
 	return temp_theta
 
 def linear_regression(theta,targets,features,learning_rate,num_iterations):
